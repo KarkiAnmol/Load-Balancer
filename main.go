9a68d80ad1fbddc2ffbc1066d70387accce5b22f
ciper <anmolkarki101@gmail.com>
2023-11-28 18:00:08 +0545

    refactor: Implement load balancing in HTTP reverse proxy

--- main.go
+++ main.go
@@ -33,12 +33,56 @@ type loadBalancer struct {
 	servers         []Server
 }
 
+func NewLoadBalancer(port string, servers []Server) *loadBalancer {
+	return &loadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
+	}
+}
+
 func handleErr(err error) {
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
 }
-func main() {
+func (s *simpleServer) Address() string {
+	return s.addr
+}
+func (s *simpleServer) IsAlive() bool {
+	return true
+}
+func (s *simpleServer) Serve(rw http.ResponseWriter, r *http.Request) {
+	s.proxy.ServeHTTP(rw, r)
+}
 
+func (lb *loadBalancer) getNextAvailableSever() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+	return server
+}
+func (lb *loadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
+	targetServer := lb.getNextAvailableSever()
+	fmt.Printf("forwarding request to address %q\n", targetServer.Address())
+	targetServer.Serve(rw, r)
+}
+
+func main() {
+	servers := []Server{
+		newSimpleServer("https://www.youtube.com"),
+		newSimpleServer("https://www.twitter.com"),
+		newSimpleServer("https://www.facebook.com"),
+	}
+	lb := NewLoadBalancer("8003", servers)
+	handleRedirect := func(rw http.ResponseWriter, r *http.Request) {
+		lb.serveProxy(rw, r)
+	}
+	http.HandleFunc("/", handleRedirect)
+	fmt.Printf("server is serving at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
 }
